1e933b5e5897ab46fe912d92851e90f08077ea35
BradGreig <brad.s.greig@gmail.com>
2019-10-31 11:38:18 +1100

    Fix issue with complex to real wisdom
    additional filename fix

--- 21CMMC_SourceCode/Programs/drive_21cmMC_streamlined.c
+++ 21CMMC_SourceCode/Programs/drive_21cmMC_streamlined.c
@@ -3043,9 +3043,60 @@ void ComputeIonisationBoxes(int sample_index, float REDSHIFT_SAMPLE, float PREV_
             }
         }
         
-        // Wisdoms will already exists, so do not need to search for them or create them (created earlier in this function if don't already exist)
         if(USE_FFTW_WISDOM) {
-            plan = fftwf_plan_dft_c2r_3d(HII_DIM, HII_DIM, HII_DIM, (fftwf_complex *)vel_gradient, (float *)vel_gradient, FFTW_WISDOM_ONLY);
+		
+		    sprintf(wisdom_filename,"../FFTW_Wisdoms/complex_to_real_%d.fftwf_wisdom",HII_DIM);
+            if(fftwf_import_wisdom_from_filename(wisdom_filename)!=0) {
+                plan = fftwf_plan_dft_c2r_3d(HII_DIM, HII_DIM, HII_DIM, (fftwf_complex *)vel_gradient, (float *)vel_gradient, FFTW_WISDOM_ONLY);
+	   		}
+			else {
+				plan = fftwf_plan_dft_c2r_3d(HII_DIM, HII_DIM, HII_DIM, (fftwf_complex *)vel_gradient, (float *)vel_gradient, FFTW_PATIENT);
+                fftwf_execute(plan);
+                
+                // Store the wisdom for later use
+                fftwf_export_wisdom_to_filename(wisdom_filename);
+                
+				// vel_gradient has now been over-written, so need to re-do the velocity gradient calculation
+                memcpy(vel_gradient, v, sizeof(fftwf_complex)*HII_KSPACE_NUM_PIXELS);
+				
+				// FFT the velocity gradient (Wisdom was already created before so no need to check)
+				plan = fftwf_plan_dft_r2c_3d(HII_DIM, HII_DIM, HII_DIM, (float *)vel_gradient, (fftwf_complex *)vel_gradient, FFTW_WISDOM_ONLY);
+                fftwf_execute(plan);
+				
+				// Now re-calculate the velocity gradient
+				for (n_x=0; n_x<HII_DIM; n_x++){
+    		        if (n_x>HII_MIDDLE)
+            		    k_x =(n_x-HII_DIM) * DELTA_K;  // wrap around for FFT convention
+            		else
+		                k_x = n_x * DELTA_K;
+            
+        		    for (n_y=0; n_y<HII_DIM; n_y++){
+                		if (n_y>HII_MIDDLE)
+                    		k_y =(n_y-HII_DIM) * DELTA_K;
+                		else
+                    		k_y = n_y * DELTA_K;
+                
+		                for (n_z=0; n_z<=HII_MIDDLE; n_z++){
+        		            k_z = n_z * DELTA_K;
+                 	   
+                    		// take partial deriavative along the line of sight
+		                    switch(VELOCITY_COMPONENT){
+        		                case 1:
+                		            *((fftwf_complex *) vel_gradient + HII_C_INDEX(n_x,n_y,n_z)) *= k_x*I/(float)HII_TOT_NUM_PIXELS;
+                        		    break;
+                        		case 3:
+                            		*((fftwf_complex *) vel_gradient + HII_C_INDEX(n_x,n_y,n_z)) *= k_z*I/(float)HII_TOT_NUM_PIXELS;
+                            		break;
+                        		default:
+                            		*((fftwf_complex *) vel_gradient + HII_C_INDEX(n_x,n_y,n_z)) *= k_y*I/(float)HII_TOT_NUM_PIXELS;
+                   	 		}
+                		}
+            		}
+        		}
+				
+                plan = fftwf_plan_dft_c2r_3d(HII_DIM, HII_DIM, HII_DIM, (fftwf_complex *)vel_gradient, (float *)vel_gradient, FFTW_WISDOM_ONLY);
+            }
+            
         }
         else {
             plan = fftwf_plan_dft_c2r_3d(HII_DIM, HII_DIM, HII_DIM, (fftwf_complex *)vel_gradient, (float *)vel_gradient, FFTW_ESTIMATE);
@@ -3534,7 +3585,7 @@ void ComputeIonisationBoxes(int sample_index, float REDSHIFT_SAMPLE, float PREV_
     if(!USE_LIGHTCONE) {
 
         if(PRINT_COEVAL_21cmBoxes) {
-            sprintf(filename, "delta_T_%f_%f_z%1.6_%i_%.0fMpc",INDIVIDUAL_ID,INDIVIDUAL_ID_2, REDSHIFT_SAMPLE, HII_DIM, BOX_LEN);
+            sprintf(filename, "delta_T_%f_%f_z%1.6f_%i_%.0fMpc",INDIVIDUAL_ID,INDIVIDUAL_ID_2, REDSHIFT_SAMPLE, HII_DIM, BOX_LEN);
             F = fopen(filename, "wb");
             mod_fwrite(delta_T, sizeof(float)*HII_TOT_NUM_PIXELS, 1, F);
             fclose(F);
